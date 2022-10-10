1374b91ed21514d9f6b73191eaf1a1e0735c7154
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-10-10 16:38:16 -0300

    Create Getters and setters Models

--- src/main/java/com/api/parkinglotcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkinglotcontrol/models/ParkingSpotModel.java
@@ -44,6 +44,83 @@ public class ParkingSpotModel implements Serializable {
     private String block;
 
 
+    public UUID getId() {
+        return id;
+    }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getResposableName() {
+        return resposableName;
+    }
+
+    public void setResposableName(String resposableName) {
+        this.resposableName = resposableName;
+    }
+
+    public String getApartament() {
+        return apartament;
+    }
+
+    public void setApartament(String apartament) {
+        this.apartament = apartament;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
 }
