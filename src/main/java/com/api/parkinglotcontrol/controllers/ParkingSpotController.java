6b4fefddf33f750e2785201c5503941a08f6bd4c
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-10-15 01:03:15 -0300

    Criando o metodo de encontrar pelo id um parkingspot

--- src/main/java/com/api/parkinglotcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkinglotcontrol/controllers/ParkingSpotController.java
@@ -14,6 +14,8 @@ import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*",maxAge = 3600)
@@ -52,6 +54,16 @@ public class ParkingSpotController {
     @GetMapping
     public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots(){
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value="id")UUID id){
+        Optional<ParkingSpotModel>parkingSpotModelOptional = parkingSpotService.findById(id);
+        if(!parkingSpotModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não encontrado :(");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
     }
 
 
